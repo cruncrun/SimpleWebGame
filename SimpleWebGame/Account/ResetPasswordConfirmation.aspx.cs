5fe84d9462087218da0cdfe2a8a7e3f02e561385
Kamil Otrebscki <crunchipsccc@gmail.com>
2019-04-01 16:21:21 +0200

    Start

---
+++ SimpleWebGame/Account/ResetPasswordConfirmation.aspx.cs
@@ -0,0 +1,8 @@
+﻿using System.Web.UI;
+
+namespace SimpleWebGame.Account
+{
+    public partial class ResetPasswordConfirmation : Page
+    {
+    }
+}
---
+++ SimpleWebGame/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SimpleWebGame.Startup))]
+namespace SimpleWebGame
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
